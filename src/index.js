99ac306752b556f405a20a8dcde566432f29d872
chinz.pp@gmail.com <chinz.pp@gmail.com>
2020-04-11 04:13:44 +0700

    Fix name

--- src/index.js
+++ src/index.js
@@ -4,15 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+const name = 'Josh Perez';
+const element = <h1>Hello, {name}</h1>;
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  element,
   document.getElementById('root')
 );
 
