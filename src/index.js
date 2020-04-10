fc6278208b21d0a969e8d9cb76b3c1ff176651d1
chinz.pp@gmail.com <chinz.pp@gmail.com>
2020-04-11 04:11:44 +0700

    Hello, world!

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <h1>Hello, world!</h1>,
   document.getElementById('root')
 );
 
