2cbf4f980cd03edce6ba976e893df24a79d40aa6
chinz.pp@gmail.com <chinz.pp@gmail.com>
2020-04-11 05:00:26 +0700

    Composing

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+function App_3() {
+  return (
+    <div>
+      <Welcome name="SaraHa" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+
 function tick() {
   //const name = "Josh Perez";
   const element = (
     <div>
       <Welcome name="Sara" />
+      <App_3 />
       <h2> It is {new Date().toLocaleTimeString()}. </h2>
     </div>
   );
