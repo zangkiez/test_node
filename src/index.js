01e164612363073ce9dfaea4d182172c81349054
chinz.pp@gmail.com <chinz.pp@gmail.com>
2020-04-11 05:21:04 +0700

    Fix Extracting Components

--- src/index.js
+++ src/index.js
@@ -14,21 +14,54 @@ function App_3() {
   );
 }
 
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img
+          className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">{props.author.name}</div>
+      </div>
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: "I hope you enjoy learning React!",
+  author: {
+    name: "Hello Kitty",
+    avatarUrl: "https://placekitten.com/g/64/64",
+  },
+};
+
+
 function tick() {
   //const name = "Josh Perez";
   const element = (
+    <div>
     <div>
       <Welcome name="Sara" />
       <App_3 />
       <h2> It is {new Date().toLocaleTimeString()}. </h2>
     </div>
+    <div><Comment date={comment.date} text={comment.text} author={comment.author} /></div></div>
   );
 
   ReactDOM.render(element, document.getElementById("root"));
-}
-
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
 }
 
 setInterval(tick, 1000);
