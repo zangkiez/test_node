e98cb338c3915e16d5f5ea57ba1b308704b7c32f
chinz.pp@gmail.com <chinz.pp@gmail.com>
2020-04-11 04:19:04 +0700

    Fix tick

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-const name = 'Josh Perez';
-const element = <h1>Hello, {name}</h1>;
+function tick() {
+  const name = "Josh Perez";
+  const element = (
+    <div>
+      <h1> Hello, {name} </h1>{" "}
+      <h2> It is {new Date().toLocaleTimeString()}. </h2>{" "}
+    </div>
+  );
 
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+  ReactDOM.render(element, document.getElementById("root"));
+}
+
+setInterval(tick, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
