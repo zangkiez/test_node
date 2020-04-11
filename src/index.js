32be812fa8cf104cc6c18acddd8f280024c6c762
chinz.pp@gmail.com <chinz.pp@gmail.com>
2020-04-12 00:19:26 +0700

    Fix css

--- src/index.js
+++ src/index.js
@@ -186,6 +186,43 @@ function Mailbox(props) {
   );
 }
 
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+
+  return (
+    <div className="warning">
+      Warning!
+    </div>
+  );
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showWarning: true};
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState(state => ({
+      showWarning: !state.showWarning
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
+      </div>
+    );
+  }
+}
+
 const messages = ['React', 'Re: React', 'Re:Re: React'];
 
 const element = (
@@ -198,6 +235,7 @@ const element = (
       <Clock />
       <h2> It is {new Date().toLocaleTimeString()}. </h2>
     </div>
+    <Page />
     <div>
       <LoginControl />
       <Comment
